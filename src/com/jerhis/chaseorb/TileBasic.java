481e8f2271fd2b7630da6420f831d1a08a98da52
Jordan <j_haack$@yahoo.com>
2013-09-02 19:42:21 -0500

    levels 3-5

--- src/com/jerhis/chaseorb/TileBasic.java
+++ src/com/jerhis/chaseorb/TileBasic.java
@@ -6,19 +6,19 @@ import org.andengine.opengl.texture.region.ITiledTextureRegion;
 public class TileBasic extends Tile {
 
 	public TileBasic(Coord coord, char charID) {
-		super(coord, 'b', charID, getImage(charID),new int[]{0},50,0);
+		super(coord, 'b', charID, getImage(charID),new int[]{0},30,0);
 	}
 
 	@Override
 	public void collision(Chaser chaser, CollisionType type) {
         if (charID == '+' && type != CollisionType.NONE){
             switch (chaser.color) {
-                case 'a': setFrames(new int[]{1,2,3,0}, 0); break;
-                case 'b': setFrames(new int[]{1,2,3,0}, 0); break;
-                case 'c': setFrames(new int[]{1,2,3,0}, 0); break;
-                case 'd': setFrames(new int[]{1,2,3,0}, 0); break;
-                case 'e': setFrames(new int[]{1,2,3,0}, 0); break;
-                case 'f': setFrames(new int[]{1,2,3,0}, 0); break;
+                case 'a': setFrames(new int[]{1,2,3,4,5,6,7,8,9,0}, 0); break;
+                case 'b': setFrames(new int[]{11,12,13,14,15,16,17,18,19,0}, 0); break;
+                case 'c': setFrames(new int[]{21,22,23,24,25,26,27,28,29,0}, 0); break;
+                case 'd': setFrames(new int[]{31,32,33,34,35,36,37,38,39,0}, 0); break;
+                case 'e': setFrames(new int[]{41,42,43,44,45,46,47,48,49,0}, 0); break;
+                case 'f': setFrames(new int[]{51,52,53,54,55,56,57,58,59,0}, 0); break;
             }
         }
 		switch (type) {
@@ -41,7 +41,7 @@ public class TileBasic extends Tile {
 	public void update(Scene s, float deltaTime) {
 		if (charID == '+') {
             animate(deltaTime);
-            if (currentFrame == 3)
+            if (currentFrame == 10)
                 setFrames(new int[]{0}, 0);
         }
 		
